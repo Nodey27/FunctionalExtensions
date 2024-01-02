72a65ae6b15725fa6647b654e024a57d6c511701
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-02 23:00:51 +0100

    Fixed function AndThen for struct result with corresponding tests

--- FunctionalExtensions/ResultExtensions/AndThenResult.vb
+++ FunctionalExtensions/ResultExtensions/AndThenResult.vb
@@ -3,12 +3,12 @@
 Namespace Functional
     Public Module AndThenResult
         <Extension()>
-        Public Function AndThen(Of T, K, E)(res As Result(Of T, E), func As Func(Of T, K)) As Result(Of K, E)
-            If res.IsOk() Then
+        Public Function AndThen(Of T, K, E)(res As Result(Of T, E), func As Func(Of T, Result(Of K, E))) As Result(Of K, E)
+            If res.IsErr() Then
                 Return Result(Of K, E).Err(res.Err())
             End If
 
-            Return Result(Of K, E).Ok(func(res.Unwrap()))
+            Return func(res.Unwrap())
         End Function
     End Module
 End Namespace
---
+++ FunctionalExtensionsTest/ResultExtensions/AndThenResultTest.vb
@@ -0,0 +1,41 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class AndThenResultTest
+
+    <TestMethod>
+    Public Sub AndThenCallsMethodOnOk()
+        Dim value = 200
+        Dim expected As Double = Math.Sqrt(value)
+        Dim sut = Result(Of Double, String).Ok(value)
+
+        Assert.AreEqual(expected, sut.
+                        AndThen(Function(x) Result(Of Double, String).Ok(Math.Sqrt(x))).
+                        Unwrap()
+       )
+    End Sub
+
+    <TestMethod>
+    Public Sub AndThenDoesNotCallMethodOnErr()
+        Dim value = 200
+        Dim expected As String = "Scary"
+        Dim sut = Result(Of Double, String).Err(expected)
+
+        Assert.AreEqual(expected, sut.
+                        AndThen(Function(x) Result(Of Double, String).Err(Math.Sqrt(x))).
+                        Err()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub AndThenReturnsErrWhenFunctionPanics()
+        Dim expected As String = "Scary"
+        Dim sut = Result(Of Double, String).Ok(2000)
+
+        Assert.AreEqual(expected, sut.
+                        AndThen(Function(x) Result(Of Double, String).Err(expected)).
+                        Err()
+        )
+    End Sub
+End Class
