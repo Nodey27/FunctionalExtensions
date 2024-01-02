e73c13388d75fe6e8c67a76cce8715ba1b470cfa
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-02 22:59:38 +0100

    Fixed function and for struct result with corresponding tests

--- FunctionalExtensions/ResultExtensions/AndResult.vb
+++ FunctionalExtensions/ResultExtensions/AndResult.vb
@@ -4,7 +4,11 @@ Namespace Functional
     Public Module AndResult
         <Extension()>
         Public Function [And](Of T, K, E)(res As Result(Of T, E), other As Result(Of K, E)) As Result(Of K, E)
-            If res.IsOk() Then
+            If res.IsErr() And other.IsErr() Or res.IsOk() And other.IsOk() Then
+                Return other
+            End If
+
+            If res.IsErr() Then
                 Return Result(Of K, E).Err(res.Err())
             End If
 
---
+++ FunctionalExtensionsTest/ResultExtensions/AndResultTest.vb
@@ -0,0 +1,43 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class AndResultTest
+
+    <TestMethod>
+    Public Sub AndReturnsEarlyError()
+        Dim expected As String = "Early error"
+        Dim x = Result(Of Integer, String).Err(expected)
+        Dim y = Result(Of Integer, String).Ok(10)
+
+        Assert.AreEqual(expected, x.And(y).Err())
+    End Sub
+
+    <TestMethod>
+    Public Sub AndReturnsLateError()
+        Dim expected As String = "Late error"
+        Dim x = Result(Of Integer, String).Err(expected)
+        Dim y = Result(Of Integer, String).Ok(10)
+
+        Assert.AreEqual(expected, y.And(x).Err())
+    End Sub
+
+    <TestMethod>
+    Public Sub AndReturnsLateErrorForDoubleErr()
+        Dim expected As String = "Late error"
+        Dim x = Result(Of Integer, String).Err("Early error")
+        Dim y = Result(Of Integer, String).Err(expected)
+
+        Assert.IsTrue(y.IsErr)
+        Assert.AreEqual(expected, x.And(y).Err())
+    End Sub
+
+    <TestMethod>
+    Public Sub AndReturnsLateValueForDoubleOk()
+        Dim expected As Integer = 1
+        Dim x = Result(Of Integer, String).Ok(100)
+        Dim y = Result(Of Integer, String).Ok(expected)
+
+        Assert.AreEqual(expected, x.And(y).Unwrap())
+    End Sub
+End Class
