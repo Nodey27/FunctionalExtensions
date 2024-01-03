d925402862184e24cfcb3f1eaecc2da314ef33f3
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-03 23:06:54 +0100

    Added tests for XOr in struct Maybe

--- FunctionalExtensions/MaybeExtensions/XOrMaybe.vb
+++ FunctionalExtensions/MaybeExtensions/XOrMaybe.vb
@@ -3,16 +3,17 @@
 Namespace Functional
     Public Module XOrMaybe
         <Extension()>
-        Public Function [XOr](Of T)(opt As Maybe(Of T), optb As Maybe(Of T)) As Maybe(Of T)
-            If opt.IsNone() And optb.IsNone Or opt.IsSome And optb.IsSome Then
+        Public Function [XOr](Of T)(opt_a As Maybe(Of T), opt_b As Maybe(Of T)) As Maybe(Of T)
+            If opt_a.IsNone() And opt_b.IsNone Or
+                opt_a.IsSome And opt_b.IsSome Then
                 Return Maybe(Of T).None()
             End If
 
-            If opt.IsSome() Then
-                Return opt
+            If opt_a.IsSome() Then
+                Return opt_a
             End If
 
-            Return optb
+            Return opt_b
         End Function
     End Module
 End Namespace
---
+++ FunctionalExtensionsTest/MaybeExtensions/XOrMaybeTest.vb
@@ -0,0 +1,55 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class XOrMaybeTest
+    <TestMethod>
+    Public Sub XOrReturnsNoneWhenBothNone()
+        Dim opt_a = Maybe(Of Integer).None()
+        Dim opt_b = Maybe(Of Integer).None()
+        Dim expected = Maybe(Of Integer).None()
+
+        Assert.AreEqual(
+            expected,
+            opt_a.XOr(opt_b)
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub XOrReturnsNoneWhenBothSome()
+        Dim opt_a = Maybe(Of Integer).Some(1)
+        Dim opt_b = Maybe(Of Integer).Some(-1)
+        Dim expected = Maybe(Of Integer).None()
+
+        Assert.AreEqual(
+            expected,
+            opt_a.XOr(opt_b)
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub XOrReturnsOptAWhenSomeOptBNone()
+        Dim value As Integer = 1
+        Dim opt_a = Maybe(Of Integer).Some(value)
+        Dim opt_b = Maybe(Of Integer).None()
+        Dim expected = Maybe(Of Integer).Some(value)
+
+        Assert.AreEqual(
+            expected,
+            opt_a.Or(opt_b)
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub XOrReturnsOptBWhenSomeOptANone()
+        Dim value As Integer = 1
+        Dim opt_b = Maybe(Of Integer).Some(value)
+        Dim opt_a = Maybe(Of Integer).None()
+        Dim expected = Maybe(Of Integer).Some(value)
+
+        Assert.AreEqual(
+            expected,
+            opt_a.Or(opt_b)
+        )
+    End Sub
+End Class
