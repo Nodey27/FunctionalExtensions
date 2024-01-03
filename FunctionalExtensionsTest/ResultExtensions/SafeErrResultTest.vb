da0c8a9ba74f34dd407ab36c9c7bdaad43b9c272
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-03 15:06:28 +0100

    Added tests for SafeErr method in struct Result

---
+++ FunctionalExtensionsTest/ResultExtensions/SafeErrResultTest.vb
@@ -0,0 +1,22 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class SafeErrResultTest
+    <TestMethod>
+    Public Sub SafeErrReturnSomeOnErr()
+        Dim message As String = "What!"
+        Dim sut = Result(Of Integer, String).Err(message)
+        Dim expected = Maybe(Of String).Some("What!")
+
+        Assert.AreEqual(expected, sut.SafeErr())
+    End Sub
+
+    <TestMethod>
+    Public Sub SafeErrReturnNoneOnOk()
+        Dim sut = Result(Of Integer, String).Ok(1)
+        Dim expected = Maybe(Of String).None()
+
+        Assert.AreEqual(expected, sut.SafeErr())
+    End Sub
+End Class
