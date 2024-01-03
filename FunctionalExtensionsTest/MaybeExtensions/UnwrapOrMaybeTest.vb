0be6acddc749db2370a7928d8890fc70e3151359
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-03 22:29:22 +0100

    Added tests for UnwrapOr in struct Maybe

---
+++ FunctionalExtensionsTest/MaybeExtensions/UnwrapOrMaybeTest.vb
@@ -0,0 +1,29 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class UnwrapOrMaybeTest
+    <TestMethod>
+    Public Sub UnwrapOrReturnsFallbackValueOnNone()
+        Dim expected As String = "This is the fallback"
+        Dim sut = Maybe(Of Object).None()
+        Dim fallback As String = expected
+
+        Assert.AreEqual(
+            expected,
+            sut.UnwrapOr(fallback)
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub UnwrapOrReturnValueOnSome()
+        Dim expected As Integer = 1
+        Dim sut = Maybe(Of Integer).Some(expected)
+        Dim fallback As Integer = -1
+
+        Assert.AreEqual(
+            expected,
+            sut.UnwrapOr(fallback)
+        )
+    End Sub
+End Class
