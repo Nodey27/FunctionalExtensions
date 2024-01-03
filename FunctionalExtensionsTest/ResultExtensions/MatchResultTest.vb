437d43e609d4cc92dd080365f8f91410cff9d816
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-03 14:14:28 +0100

    Added test for match with return for struct result

---
+++ FunctionalExtensionsTest/ResultExtensions/MatchResultTest.vb
@@ -0,0 +1,48 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class MatchResultTest
+    <TestMethod>
+    Public Sub MatchReturnsValueOnOk()
+        Dim expected As Integer = 1002
+        Assert.AreEqual(
+            expected,
+            Result(Of Integer, String).
+                Ok(expected).
+                Match(
+                    Function(x) x,
+                    Function(err) -1
+                )
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub MatchReturnsValueOnErr()
+        Dim expected As Integer = 1002
+        Assert.AreEqual(
+            expected,
+            Result(Of Integer, String).
+                Err("Some error").
+                Match(
+                    Function(x) x,
+                    Function(err) expected
+                )
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub MatchReturnsValueOnErrInChain()
+        Dim expected As Integer = 1002
+        Assert.AreEqual(
+            expected,
+            Result(Of Integer, String).
+                Ok(-1).
+                Assert(Function(x) expected < 0, Function(err) err).
+                Match(
+                    Function(x) x,
+                    Function(err) expected
+                )
+        )
+    End Sub
+End Class
