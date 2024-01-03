14b1741dd1c05c82d1be8ac7baa1463ece9aaa92
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-03 13:38:56 +0100

    Added tests for the method map in struct Result

---
+++ FunctionalExtensionsTest/ResultExtensions/MapResultTest.vb
@@ -0,0 +1,48 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class MapResultTest
+    <TestMethod>
+    Public Sub MapReturnsMappedValueOnOk()
+        Dim value As Integer = 4
+        Dim expected As Double = Math.Sqrt(value)
+
+        Assert.AreEqual(
+            expected,
+            Result(Of Double, String).
+                Ok(value).
+                Map(Function(x) Math.Sqrt(x)).
+                Unwrap()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub MapReturnsMappedValueChainOnOk()
+        Dim value As Integer = 4
+        Dim expected As Double = Math.Sqrt(Math.Sqrt(value))
+
+        Assert.AreEqual(
+            expected,
+            Result(Of Double, String).
+                Ok(value).
+                Map(Function(x) Math.Sqrt(x)).
+                Map(Function(x) Math.Sqrt(x)).
+                Unwrap()
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub MapReturnsFirstErrOnChainErr()
+        Dim expected As String = "This is the earliest error"
+
+        Assert.AreEqual(
+            expected,
+            Result(Of Double, String).
+                Err(expected).
+                Map(Function(x) Math.Sqrt(x)).
+                Map(Function(x) Math.Sqrt(x)).
+                Err()
+        )
+    End Sub
+End Class
