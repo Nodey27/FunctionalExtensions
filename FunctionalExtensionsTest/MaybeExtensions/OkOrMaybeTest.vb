76e9c57a12097c6cb08837d7a5513e9326b5facd
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-03 22:01:35 +0100

    Added tests for OkOr for struct Maybe

---
+++ FunctionalExtensionsTest/MaybeExtensions/OkOrMaybeTest.vb
@@ -0,0 +1,25 @@
+﻿Imports FunctionalExtensions.Functional
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class OkOrMaybeTest
+    <TestMethod>
+    Public Sub OkOrReturnsErrWhenNone()
+        Dim expected As String = "Maybe does not contain any value, Oops :)"
+        Assert.AreEqual(
+            Result(Of Object, String).Err(expected),
+            Maybe(Of Object).None().
+                OkOr(expected)
+        )
+    End Sub
+
+    <TestMethod>
+    Public Sub OkOrReturnsOkWhenSome()
+        Dim expected As Integer = 1
+        Assert.AreEqual(
+            Result(Of Integer, String).Ok(expected),
+            Maybe(Of Integer).Some(expected).
+                OkOr("Does not matter")
+        )
+    End Sub
+End Class
