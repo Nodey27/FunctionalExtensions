53cd99b20115e537b1b39445c8ad3b57b8b435de
JustinKasteleijn <48954608+JustinKasteleijn@users.noreply.github.com>
2024-01-04 22:42:11 +0100

    Changed tests for Try to be more clear and added the base structure to the examples in readme document

--- FunctionalExtensionsTest/Result/Functions/TryResultTest.vb
+++ FunctionalExtensionsTest/Result/Functions/TryResultTest.vb
@@ -4,33 +4,44 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass>
 Public Class TryResultTest
 
+    Private Const expectedMessage As String = "Can not sqrt negative integers"
+
+    Public Function CustomSqrt(x As Integer) As Double
+        If x < 0 Then
+            Throw New Exception(expectedMessage)
+        Else
+            Return Math.Sqrt(x)
+        End If
+    End Function
+
     <TestMethod>
-    Public Sub TryReturnsValueWhenNoException()
-        Dim expected As Integer = 1
-        Dim fun As Func(Of Integer) = Function() expected
+    Public Sub ExampleNoException()
+        Dim value As Integer = 15
+        Dim expected As Result(Of Double, String) =
+            Result(Of Double, String).Ok(value:=CustomSqrt(value))
+        Dim res As Result(Of Double, String) =
+            Result(Of Double, String).
+                Try(
+                    func:=Function() CustomSqrt(value),
+                    onError:=Function(exception) exception.Message
+                )
 
-        Dim res As Result(Of Integer, String) = Result(Of Integer, String).Try(
-            fun,
-            Function(err) "Err"
-        )
-
-        Assert.AreEqual(expected, res.Unwrap())
+        Assert.AreEqual(expected, res)
     End Sub
 
     <TestMethod>
-    Public Sub TryReturnsErrWhenException()
-        Dim expected As String = "Exception has occured!"
-        Dim exc As Exception = New Exception(message:=expected)
-        Dim fun As Func(Of Exception, String) = Function(err) err.Message
+    Public Sub ExampleCatchException()
+        Dim value As Integer = -1
+        Dim expected As Result(Of Double, String) =
+            Result(Of Double, String).Err(errorValue:=expectedMessage)
 
-        Dim res As Result(Of Integer, String) = Result(Of Integer, String).Try(
-            Function()
-                Throw exc
-                Return 10
-            End Function,
-            fun
-        )
+        Dim res As Result(Of Double, String) =
+            Result(Of Double, String).
+                Try(
+                    func:=Function() CustomSqrt(value),
+                    onError:=Function(exception) exception.Message
+                )
 
-        Assert.AreEqual(expected, res.Err())
+        Assert.AreEqual(expected, res)
     End Sub
 End Class
